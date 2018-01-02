b098c40fcd82eee75852624753c00cd0739b6683
Ashwanth <achu7148@gmail.com>
2018-01-02 17:38:18 +0530

    Added Dependency, Program
    Added Apache poi dependency
    Added csvtoexcelread program

---
+++ src/main/java/ashwanth/programs/csvtoexcelconvertor.java
@@ -0,0 +1,80 @@
+package ashwanth.programs;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+
+
+public class csvtoexcelconvertor {
+
+    public static void main(String args[]) throws IOException
+    {
+        ArrayList arList=null;
+        ArrayList al=null;
+        String fName = "D:\\a.csv";
+        String thisLine;
+        int count=0;
+        FileInputStream fis = new FileInputStream(fName);
+        DataInputStream myInput = new DataInputStream(fis);
+        int i=0;
+        arList = new ArrayList();
+        while ((thisLine = myInput.readLine()) != null)
+        {
+            al = new ArrayList();
+            String strar[] = thisLine.split(",");
+            for(int j=0;j<strar.length;j++)
+            {
+                al.add(strar[j]);
+            }
+            arList.add(al);
+            System.out.println();
+            i++;
+        }
+
+        try
+        {
+            HSSFWorkbook hwb = new HSSFWorkbook();
+            HSSFSheet sheet = hwb.createSheet("new sheet");
+            for(int k=0;k<arList.size();k++)
+            {
+                ArrayList ardata = (ArrayList)arList.get(k);
+                HSSFRow row = sheet.createRow((short) 0+k);
+                for(int p=0;p<ardata.size();p++)
+                {
+                    HSSFCell cell = row.createCell((short) p);
+                    String data = ardata.get(p).toString();
+                    if(data.startsWith("=")){
+                        cell.setCellType(Cell.CELL_TYPE_STRING);
+                        data=data.replaceAll("\"", "");
+                        data=data.replaceAll("=", "");
+                        cell.setCellValue(data);
+                    }else if(data.startsWith("\"")){
+                        data=data.replaceAll("\"", "");
+                        cell.setCellType(Cell.CELL_TYPE_STRING);
+                        cell.setCellValue(data);
+                    }else{
+                        data=data.replaceAll("\"", "");
+                        cell.setCellType(Cell.CELL_TYPE_NUMERIC);
+                        cell.setCellValue(data);
+                    }
+                    //*/
+                    // cell.setCellValue(ardata.get(p).toString());
+                }
+                System.out.println();
+            }
+            FileOutputStream fileOut = new FileOutputStream("D:\\b.xls");
+            hwb.write(fileOut);
+            fileOut.close();
+            System.out.println("Your excel file has been generated");
+        } catch ( Exception ex ) {
+            ex.printStackTrace();
+        } //main method ends
+    }
+}
