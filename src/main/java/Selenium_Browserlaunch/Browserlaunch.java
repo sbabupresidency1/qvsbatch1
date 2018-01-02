3acdf7f5cbafa6817ec731f88463d23acea491bf
Ashwanth <achu7148@gmail.com>
2018-01-02 17:14:18 +0530

    Added Dependency, Added a program
    Added chrome dependency
    Added Browser launching in chrome

---
+++ src/main/java/Selenium_Browserlaunch/Browserlaunch.java
@@ -0,0 +1,31 @@
+package Selenium_Browserlaunch;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Browserlaunch {
+	 public static void main(String[] args) throws InterruptedException  {
+		 System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe"); //Setting up chrome driver
+	      WebDriver driver = new ChromeDriver();
+		 
+		 driver.get("http://prince2.sensiple.com/prince-web/ems/login/view");//Launch Browser
+		 
+		 driver.manage().window().maximize();//Maximize the window
+		 
+		 //Enter Username, Password and login
+		 driver.findElement(By.id("j_username")).sendKeys("2305");
+	     driver.findElement(By.id("j_password")).sendKeys("achu7148");
+		 driver.findElement(By.xpath(".//*[@id='loginForm']/button")).click();
+		 driver.findElement(By.id("swipeDetailsId")).click();
+		 Thread.sleep(3000);
+		 driver.findElement(By.id("get-resource-assignment")).click();
+		
+		 Thread.sleep(6000);
+		 
+		 
+		 
+		 driver.close();
+	 }
+}
