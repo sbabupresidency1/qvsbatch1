9a583c8e9429e94d3b94cfd04ca5ed380d59f3d6
parkavi04 <parkavi.umapathy@sensiple.com>
2018-01-03 09:51:50 +0530

    Exception Handling
    A program for exception handling which holds switch in final block

---
+++ src/main/java/parkavi1/ExceptionHandling.java
@@ -0,0 +1,58 @@
+package parkavi1;
+
+	import java.util.Scanner;
+
+	public class ExceptionHandling {
+
+	
+		@SuppressWarnings("resource")
+		public static void main(String[] args) {
+			int a, b, c, res;
+	char op;
+			Scanner in = new Scanner(System.in);
+			System.out.println("Get two integers : " );
+
+			a = in.nextInt();
+			b = in.nextInt();
+
+			try {
+				c = a / b;
+				System.out.println("Result=" + c);
+			}		catch (ArithmeticException e) {
+				System.out.println("Exception found : Divideby zero");
+			}
+			finally
+			{
+			System.out.println("Enter the operator");
+			op=in.next().charAt(0);
+				switch(op)
+				{
+				case '+':
+					res = a + b;
+					System.out.println("" +res);
+					break;
+				case '-':
+					res = a - b;
+					System.out.println("" +res);
+
+					break;
+				case '*':
+					res = a * b;
+					System.out.println("" +res);
+
+					break;
+				case '/':
+					res = a / b;
+					System.out.println("" +res);
+
+					break;
+				default:
+					System.out.println("Invalid operator");
+					break;
+						
+				}
+			}
+		}
+	}
+
+
