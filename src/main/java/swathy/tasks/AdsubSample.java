d21ab15aa20d495ce1d680222c18b291fcfe6336
swathy01 <34912646+swathy01@users.noreply.github.com>
2018-01-04 09:44:11 +0530

    Single package for three tasks.
    created swathy.tasks package that consists of the five classes.

--- src/main/java/swathy/tasks/AdsubSample.java
+++ src/main/java/swathy/tasks/AdsubSample.java
@@ -1,10 +1,12 @@
-package task2switchinherit;
+package swathy.tasks;
 
 public class AdsubSample {
 	/*
 	 * Created By: Swathy.S 
 	 * Created Date: 27th December 2017 
 	 * Created Time: 5:00PM
+	 * Modified Date: 3rd January 2018
+	 * Modified Time: 5:04PM
 	 * Concept Used: Switch & Inheritance 
 	 * Task Number: 2
 	 */
