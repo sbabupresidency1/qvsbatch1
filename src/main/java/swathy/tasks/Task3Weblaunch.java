318590105a2186a22dc82ca1acebfa7400828424
swathy01 <34912646+swathy01@users.noreply.github.com>
2018-01-16 14:34:10 +0530

    annotations in testng

--- src/main/java/swathy/tasks/Task3Weblaunch.java
+++ src/main/java/swathy/tasks/Task3Weblaunch.java
@@ -7,9 +7,14 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Task3Weblaunch {
 
 	/*
-	 * Created By: Swathy.S Created Date: 29th December '17 Created Time: 4:00PM
-	 * Modified Date:3rd January '18 Modified Time: 1:48 PM Concept Used: Chrome
-	 * browser launch(Webmail) Task Number: 3
+	 * Created By: Swathy.S 
+	 * Created Date: 29th December '17 
+	 * Created Time: 4:00PM
+	 * Modified Date:3rd January '18 
+	 * Modified Time: 1:48 PM 
+	 * Concept Used: Chrome
+	 * browser launch(Webmail) 
+	 * Task Number: 3
 	 */
 
 	public static void main(String[] args) throws InterruptedException {
@@ -52,7 +57,7 @@ public class Task3Weblaunch {
 		driver.findElement(By.name("username")).sendKeys("swathy.suresh@sensiple.com");
 		driver.findElement(By.name("password")).sendKeys("SKSsuvi@1510");
 		driver.findElement(By.name("mysubmit")).click();
-		Thread.sleep(4000);
+	//	Thread.sleep(4000);
 		driver.findElement(By.id("DWT75_button3_title")).click();
 		Thread.sleep(2000);
 		driver.findElement(By.id("zli__CLV-main__-1664")).click();
