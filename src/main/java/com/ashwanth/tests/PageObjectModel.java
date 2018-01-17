b415768194ec527b8011fc0af292d2b0f7790cdd
Ashwanth <achu7148@gmail.com>
2018-01-17 16:45:58 +0530

    Check Annotations flow

--- src/main/java/com/ashwanth/tests/PageObjectModel.java
+++ src/main/java/com/ashwanth/tests/PageObjectModel.java
@@ -4,8 +4,8 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -14,13 +14,14 @@ import com.ashwanth.pages.LogIn_Page;
 
 public class PageObjectModel {
 	
-	WebDriver driver = new ChromeDriver();
+	WebDriver driver;
 	
 	@BeforeSuite
 	public void LaunchBrowser() {
 		
-		 System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe"); //Setting up chrome driver
-		   
+		 System.setProperty("webdriver.chrome.driver", "C:\\workspace\\qvsbatch1\\SelDrivers\\chromedriver.exe"); //Setting up chrome driver
+		 driver = new ChromeDriver();  
+		 
 	     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
 	     driver.get("http://prince2.sensiple.com/prince-web/ems/login/view");
@@ -28,7 +29,7 @@ public class PageObjectModel {
 	     driver.manage().window().maximize();	 
 	}
 	
-	@Test
+	@AfterSuite
 	public void Login() {
 		   
 	     LogIn_Page.txtbx_UserName(driver).sendKeys("2305");
@@ -36,10 +37,7 @@ public class PageObjectModel {
 	     LogIn_Page.txtbx_Password(driver).sendKeys("achu7148");
 	 
 	     LogIn_Page.btn_LogIn(driver).click();
-	}
-	   
-	@AfterSuite
-	 public void Exit() {
+
 	     System.out.println(" Login Successfull, now it is the time to Log Off.");
 	 
 	     driver.quit();
