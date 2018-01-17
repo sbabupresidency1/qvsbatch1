d332996383f4dfd593609124fd6b10f60293a4a5
parkavi04 <parkavi.umapathy@sensiple.com>
2018-01-17 14:13:20 +0530

    removed some header files

--- src/main/java/swathy/tasks/Task4AnnotSample.java
+++ src/main/java/swathy/tasks/Task4AnnotSample.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 //import org.testng.annotations.BeforeMethod;
 //import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.openqa.selenium.Alert;
+//import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
