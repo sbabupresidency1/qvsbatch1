f43be7810875193f4be4e2a8f5b57742c55b7314
RamyaNagalakshmi <34912554+RamyaNagalakshmi@users.noreply.github.com>
2018-01-17 10:58:40 +0530

    Added documentation

--- src/main/java/ramyar/common_method.java
+++ src/main/java/ramyar/common_method.java
@@ -7,36 +7,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-public class common_method{
+/*
+ * Created By:   R.Ramya Nagalakshmi 
+ * Created Date: 12/1/18
+ * Description:  Common method to open URL & Browser
+ */ 
+
+public class common_method {
 	static WebDriver driver;
-	
-	public static WebDriver OpenApp(String BrowserName, String url){
+ //Common method 
+	public static WebDriver OpenApp(String BrowserName, String url) {
 		fn_LaunchBrowser(BrowserName);
 		fn_OpenURL(url);
-		
+
 		return driver;
-		}
-		public static void fn_OpenURL(String url){
+	}
+//To invoke URL
+	public static void fn_OpenURL(String url) {
 		driver.get("http://mail.sensiple.com/");
 		driver.manage().window().maximize();
-		}
-		 
-		public static WebDriver fn_LaunchBrowser(String browsername){
-		if(browsername=="CH"){
-		System.setProperty("webdriver.chrome.driver","D:\\Selenium Jar files\\chromedriver_win32\\chromedriver.exe");
-		driver= new ChromeDriver();
-		}else if(browsername=="FF"){
-			System.setProperty("webdriver.chrome.driver","D:\\Selenium jar files\\geckodriver.exe");	
-		driver= new FirefoxDriver();
-		}else if(browsername=="IE"){
-		System.setProperty("webdriver.ie.driver", "D:\\Selenium Jar files\\IEDriverServer_x64_3.8.0\\IEDriverServer.exe");
-		driver= new InternetExplorerDriver();
+	}
+//To launch browser
+	public static WebDriver fn_LaunchBrowser(String browsername) {
+		if (browsername == "CH") {
+			System.setProperty("webdriver.chrome.driver",
+					"D:\\Selenium Jar files\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browsername == "FF") {
+			System.setProperty("webdriver.chrome.driver", "D:\\Selenium jar files\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browsername == "IE") {
+			System.setProperty("webdriver.ie.driver",
+					"D:\\Selenium Jar files\\IEDriverServer_x64_3.8.0\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
 		}
 		driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
 		return driver;
-		}
-		
+	}
 
-		 
-		}
-		
+}
