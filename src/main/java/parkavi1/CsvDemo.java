dde952da1d4c1c57eeb1942d22fc7ca8add99694
parkavi04 <parkavi.umapathy@sensiple.com>
2018-01-18 14:05:11 +0530

    Edited few lines

--- src/main/java/parkavi1/CsvDemo.java
+++ src/main/java/parkavi1/CsvDemo.java
@@ -5,20 +5,18 @@ import java.io.FileReader;
 import java.util.Arrays;
 
 public class CsvDemo{
-
-    
-    public static void main(String[] args) throws Exception {
-                String x= ",";
-        BufferedReader br = new BufferedReader(new FileReader("C:\\SeleniumBackUp\\csvdemo.csv"));
-        String line = br.readLine();
-        if(line!= null){
-             String[] b = line.split(x);
-             System.out.println(Arrays.toString(b));
-      
-        }
-        br.close();
-
-  }
+	public static void main(String[] args) throws Exception
+	{
+		String x= ",";
+		FileReader f=new FileReader("C:\\SeleniumBackUp\\csvdemo.csv");
+		BufferedReader br = new BufferedReader(f);//(new FileReader("C:\\SeleniumBackUp\\csvdemo.csv"));
+		String line = br.readLine();
+		if(line!= null){
+			String[] b = line.split(x);
+			System.out.println(Arrays.toString(b));
+		}
+		br.close();
+	}
 }
 
 
